4b2ad3c2c6703dcd1ed967e38f44df47a9a57d14
nigaknight <1275647040@qq.com>
2019-07-09 22:58:50 +0800

    通过反射获取构造方法

--- Project/javase/src/reflection/Demo02.java
+++ Project/javase/src/reflection/Demo02.java
@@ -8,7 +8,7 @@ import java.lang.reflect.Constructor;
     通过反射获取构造方法并使用
 */
 public class Demo02 {
-    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException {
+    public static void main(String[] args) throws Exception {
         Class c=Class.forName("reflection.Person");
 
         System.out.println("通过getConstructors方法获取公共的构造函数");
@@ -23,7 +23,17 @@ public class Demo02 {
             System.out.println(constructor);
         }
 
-        System.out.println("获取单个构造方法");
+        // 按参数个数和类型获取构造方法
+        System.out.println("获取单个无参构造方法");
         Constructor constructor = c.getConstructor();
+        System.out.println(constructor);
+
+        System.out.println("获取三个参数的构造方法");
+        Class c2=Class.forName("reflection.Person");
+        Constructor constructor1 = c.getConstructor(String.class, int.class, String.class);
+        System.out.println(constructor1);
+        System.out.println("通过带参构造方法创建对象");
+        Object obj=constructor1.newInstance("邓紫棋",27,"香港");
+        System.out.println(obj);
     }
 }
